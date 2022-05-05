4b6cb906576cc0041ed2d5b05011f42b5ac03179
LVala <84684231+LVala@users.noreply.github.com>
2022-05-05 14:16:41 +0200

    Add ability to select desired area

--- src/motion_detection.py
+++ src/motion_detection.py
@@ -1,8 +1,9 @@
-from copyreg import constructor
+import numpy as np
 import cv2
 
-CONT_AR = 1000  # TODO change sensitivity
-DEBUG = True  # TODO debug mode
+CONT_AR = 10  # TODO change sensitivity
+DEBUG = False  # TODO debug mode
+USE_CUSTOM_AREA = True
 
 cap = cv2.VideoCapture("test.avi")  # TODO different video sources
 
@@ -11,14 +12,22 @@ ret, frame1 = cap.read()
 gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 blur1 = cv2.GaussianBlur(gray1, (21, 21), 0)
 
+# select area of interest
+roi = cv2.selectROI("Select area or interest", frame1)
+cv2.destroyWindow("Select area or interest")
+black = np.zeros((frame1.shape[0], frame1.shape[1], 3), np.uint8)
+black1 = cv2.rectangle(black,(roi[0], roi[1]),(roi[0]+roi[2], roi[1]+roi[3]),(255, 255, 255), -1)
+gray = cv2.cvtColor(black,cv2.COLOR_BGR2GRAY)
+ret,b_mask = cv2.threshold(gray,127,255, 0)
+
 while cap.isOpened():
-    # TODO select area of image
     ret, frame2 = cap.read()
     gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
     blur2 = cv2.GaussianBlur(gray2, (21, 21), 0)
 
     frame_diff = cv2.absdiff(blur1, blur2)
     thresh = cv2.threshold(frame_diff, 25, 255, cv2.THRESH_BINARY)[1]
+    if USE_CUSTOM_AREA: thresh = cv2.bitwise_and(thresh, thresh, mask=b_mask)
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
